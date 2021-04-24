90c58c6d5969d35bb901aae46c4ebcfb09cc2b0d
4ss1s <assisguitar23@gmail.com>
2021-04-23 21:50:45 -0300

    feat: first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="node_modules/bootstrap/dist/css/bootstrap.css">
+</head>
+<body>
+  <div class="container">
+    <form action="#" method="get">
+      <div class="row">
+        <div class="col-sm-6 col-md-12">
+          <button class="btn btn-success">ok</button>
+        </div>
+      </div>
+    </form>
+  </div>
+  <script src="node_modules/jquery/dist/jquery.js"></script>
+  <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
+</body>
+</html>
